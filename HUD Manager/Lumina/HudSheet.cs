5fd92ae30ed521984f3b0e527214ed0c16a735f0
Anna Clemens <git@annaclemens.io>
2021-04-05 15:02:06 -0400

    refactor: update to api level 3

--- HUD Manager/Lumina/HudSheet.cs
+++ HUD Manager/Lumina/HudSheet.cs
@@ -4,19 +4,20 @@ using Lumina.Text;
 
 namespace HUD_Manager.Lumina {
     [Sheet("Hud")]
-    public class HudSheet : IExcelRow {
-        public uint RowId { get; set; }
-        public uint SubRowId { get; set; }
+    public class HudSheet : ExcelRow {
         public string Name { get; set; } = null!;
         public string ShortName { get; set; } = null!;
         public string ShorterName { get; set; } = null!;
 
-        public void PopulateData(RowParser parser, global::Lumina.Lumina lumina, Language language) {
+        public override void PopulateData(RowParser parser, global::Lumina.GameData lumina, Language language) {
             this.RowId = parser.Row;
             this.SubRowId = parser.SubRow;
             this.Name = parser.ReadColumn<SeString>(0);
             this.ShortName = parser.ReadColumn<SeString>(1);
             this.ShorterName = parser.ReadColumn<SeString>(2);
+
+            this.SheetLanguage = language;
+            this.SheetName = parser.Sheet.Name;
         }
     }
 }
