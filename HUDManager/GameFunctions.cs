63dba2358088c5e49490549e83f291ebb81ef1d6
nebel <9887+nebel@users.noreply.github.com>
2026-06-08 02:26:31 +0900

    Simplify GameFunctions

--- HUDManager/GameFunctions.cs
+++ HUDManager/GameFunctions.cs
@@ -1,47 +1,33 @@
-﻿using FFXIVClientStructs.FFXIV.Component.GUI;
-using System;
+﻿using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 using System.Runtime.InteropServices;
 
 namespace HUDManager;
 
-public class GameFunctions
-{
-    private delegate byte UpdateAddonPositionDelegate(IntPtr raptureAtkUnitManager, IntPtr addon, byte clicked);
-    private readonly UpdateAddonPositionDelegate _updateAddonPosition;
+public class GameFunctions {
+    private delegate byte UpdateAddonPositionDelegate(nint raptureAtkUnitManager, nint addon, byte clicked);
+    private readonly UpdateAddonPositionDelegate updateAddonPosition;
 
     private Plugin Plugin { get; }
 
-    public GameFunctions(Plugin plugin)
-    {
+    public GameFunctions(Plugin plugin) {
         Plugin = plugin;
 
         var updatePositionPtr = Plugin.SigScanner.ScanText("E8 ?? ?? ?? ?? 48 8B 8B ?? ?? ?? ?? 33 D2 48 8B 01 FF 90 ?? ?? ?? ??");
-        _updateAddonPosition = Marshal.GetDelegateForFunctionPointer<UpdateAddonPositionDelegate>(updatePositionPtr);
+        updateAddonPosition = Marshal.GetDelegateForFunctionPointer<UpdateAddonPositionDelegate>(updatePositionPtr);
     }
 
-    public unsafe void SetAddonPosition(string uiName, short x, short y)
-    {
-        var addon = Plugin.GameGui.GetAddonByName(uiName);
-        if (addon == IntPtr.Zero) {
+    public unsafe void SetAddonPosition(string uiName, short x, short y) {
+        var addonPtr = Plugin.GameGui.GetAddonByName(uiName);
+        if (addonPtr.IsNull)
             return;
-        }
 
-        var addonPtr = (AtkUnitBase*)addon.Address;
+        var addon = (AtkUnitBase*)addonPtr.Address;
+        var raptureAtkUnitManager = RaptureAtkUnitManager.Instance();
 
-        var baseUi = Plugin.GameGui.GetUIModule().Address;
-        var manager = Marshal.ReadIntPtr(baseUi + 0x20);
-
-        _updateAddonPosition(
-            manager,
-            addon,
-            1
-        );
-        addonPtr->SetPosition(x, y);
-        _updateAddonPosition(
-            manager,
-            addon,
-            0
-        );
+        updateAddonPosition((nint)raptureAtkUnitManager, addonPtr, 1);
+        addon->SetPosition(x, y);
+        updateAddonPosition((nint)raptureAtkUnitManager, addonPtr, 0);
     }
 
     public Vector2<short>? GetAddonPosition(string uiName) {
